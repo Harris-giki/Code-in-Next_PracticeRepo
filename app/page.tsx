84cf0bb083f445abc5b245fc7f71dae36d021666
harris-giki <harris-giki@gmail.com>
2025-06-12 13:01:03 +0500

    Routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Users from "./users/page"; // Adjusted import path
 
-const page = () => {
+const Page = () => {
   return (
     <div>
       Hello
       <br />
-      <button className="btn btn-primary">Helloworld</button>
+      <button className="btn btn-primary">Hello world</button>
+      <Users />
     </div>
   );
 };
 
-export default page;
+export default Page;
